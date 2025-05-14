89ae5459765f89de3251f06a3428d9dc009c3859
DenWaritthon <waritt10@gmail.com>
2025-05-14 21:00:47 +0700

    save log data .yaml

--- src/ur5_management/scripts/set_target.py
+++ src/ur5_management/scripts/set_target.py
@@ -11,18 +11,33 @@ class SetTarget(Node):
         # Create a service server
         self.create_service(GetTarget, '/get_target', self.get_target_callback)
 
+        # Variablesse
+        self.call_count = 0
+
         self.get_logger().info(f'Set Target Node has been started')
 
     def get_target_callback(self, request:GetTarget.Request, response:GetTarget.Response):
         self.get_logger().info(f'service request received')
         if request.call:
-            response.pick_target.position.x = 0.5
-            response.pick_target.position.y = 0.0
-            response.pick_target.position.z = -0.1
+            self.call_count += 1
 
-            response.place_target.position.x = 0.0
-            response.place_target.position.y = 0.5
-            response.place_target.position.z = 0.3
+            if self.call_count == 1:
+                response.pick_target.position.x = 0.5
+                response.pick_target.position.y = 0.0
+                response.pick_target.position.z = -0.1
+
+                response.place_target.position.x = 0.0
+                response.place_target.position.y = 0.5
+                response.place_target.position.z = 0.3
+            else:
+                response.pick_target.position.x = 0.5
+                response.pick_target.position.y = 0.0
+                response.pick_target.position.z = 0.1
+
+                response.place_target.position.x = 0.0
+                response.place_target.position.y = -0.5
+                response.place_target.position.z = 0.3
+
             self.get_logger().info(f'Setting target success')
         else:
             self.get_logger().error(f'Setting target failed')
--- src/ur5_management/scripts/target_management.py
+++ src/ur5_management/scripts/target_management.py
@@ -4,12 +4,21 @@ import rclpy
 from rclpy.node import Node
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from ament_index_python import get_package_share_directory
 
 from ur5_interfaces.srv import *
+import yaml
+import os
 
 class TargetManagement(Node):
     def __init__(self):
         super().__init__('target_management')
+
+        # Set up .yaml file
+        pkg_name = 'ur5_management'
+        pkg_share_path = get_package_share_directory(pkg_name)
+        ws_path, _ = pkg_share_path.split('install')
+        self.yaml_path = os.path.join(ws_path ,'src', pkg_name , 'data_logger', 'move_log.yaml')
 
         # Create a service server
         self.create_service(Start, '/start', self.start_callback)
@@ -26,32 +35,88 @@ class TargetManagement(Node):
             self.get_logger().info('service /get_target not available, waiting again...')
         self.get_logger().info('service /get_target is available')
 
+        while not self.moveit_target_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('service /target not available, waiting again...')
+        self.get_logger().info('service /target is available')
+
+        # Create a timer
+        self.timer = self.create_timer(0.1, self.timer_loop)
+
         self.get_logger().info(f'Target Management Node has been started')
 
         # Variables
+        self.start = False
+        self.move_done = False
+        self.curent_pose = [0.0, 0.0, 0.0]
+        self.move_count_max = 2
+        self.move_count = 0
+        self.move_step = 0
+        
+        self.move_pick = 0.0
+        self.move_place = 0.0
+
         self.pick_target = [0.0, 0.0, 0.0]
         self.place_target = [0.0, 0.0, 0.0]
+
+        self.target_list = []
         
 
     def start_callback(self, request:Start.Request, response:Start.Response):
-        self.get_logger().info(f'Start call')
-        # Call get target
-        self.call_get_target()
-        self.call_moveit_target()
+        # self.get_logger().info(f'Start call')
+        if request.start:
+            self.start = True
+            self.move_done = True
+            self.get_logger().info(f'Start: {self.start}')
+            self.call_get_target()
+            self.set_target_list()
         return response
     
     def report_callback(self, request:MoveItReport.Request, response:MoveItReport.Response):
-        self.get_logger().info(f'Report call')
-
-        self.get_logger().info(f'Move done: {request.move_done}')
-        self.get_logger().info(f'Curent pose: {request.current_pose}')
+        # self.get_logger().info(f'Report call')
+        if request.move_done:
+            self.move_done = True
+            self.curent_pose[0] = request.current_pose.position.x
+            self.curent_pose[1] = request.current_pose.position.y
+            self.curent_pose[2] = request.current_pose.position.z
+            self.get_logger().info(f'Move done: {self.move_done}')
+            if self.move_step == 2:
+                self.save_yaml(pick=True)
+            elif self.move_step == 5:
+                self.save_yaml(place=True)
+                
         return response
     
-    def call_moveit_target(self):
+    def save_yaml(self, pick=False, place=False):
+
+        if not os.path.exists(self.yaml_path):
+            with open(self.yaml_path, 'w') as file:
+                yaml.dump({}, file)
+
+        with open(self.yaml_path, 'r') as file:
+            value = yaml.safe_load(file) or {}
+
+        if pick:
+            key = f'Pick target {self.move_count}'
+            value[key] = {
+            'target': self.pick_target,
+            'real_move': self.curent_pose
+            }
+
+        elif place:
+            key = f'Place target {self.move_count}'
+            value[key] = {
+            'target': self.place_target,
+            'real_move': self.curent_pose
+            }
+
+        with open(self.yaml_path, 'w') as file:
+            yaml.dump(value, file)
+    
+    def call_moveit_target(self, target):
         request = MoveItTarget.Request()
-        request.target.position.x = self.pick_target[0]
-        request.target.position.y = self.pick_target[1]
-        request.target.position.z = self.pick_target[2]
+        request.target.position.x = target[0]
+        request.target.position.y = target[1]
+        request.target.position.z = target[2]
 
         result = self.moveit_target_client.call(request)
 
@@ -70,9 +135,45 @@ class TargetManagement(Node):
         self.place_target[1] = result.place_target.position.y
         self.place_target[2] = result.place_target.position.z
 
+        self.move_count += 1
+
+        self.get_logger().info(f'Move count: {self.move_count}')
         self.get_logger().info(f'Pick target: {self.pick_target}')
         self.get_logger().info(f'Place target: {self.place_target}')
         self.get_logger().info(f'Get target success')
+
+    def set_target_list(self):
+        upper_pick_target = [self.pick_target[0], self.pick_target[1], self.pick_target[2] + 0.2]
+        upper_place_target = [self.place_target[0], self.place_target[1], self.place_target[2] + 0.2]
+
+        self.target_list.append(upper_pick_target)
+        self.target_list.append(self.pick_target)
+        self.target_list.append(upper_pick_target)
+        self.target_list.append(upper_place_target)
+        self.target_list.append(self.place_target)
+        self.target_list.append(upper_place_target)
+
+        # self.get_logger().info(f'Target list: {self.target_list}')
+    
+    def timer_loop(self):
+        if self.start and len(self.target_list) > 0:
+            if self.move_done:
+                self.move_done = False
+                self.move_step += 1
+                target = self.target_list.pop(0)
+                self.get_logger().info(f'Current target: {target}')
+                self.call_moveit_target(target)
+
+        elif self.start and len(self.target_list) == 0 and self.move_count < self.move_count_max:
+            self.move_step = 0
+            self.call_get_target()
+            self.set_target_list()
+        elif self.start and len(self.target_list) == 0 and self.move_count >= self.move_count_max:
+            self.start = False
+            self.move_step = 0
+            self.get_logger().info(f'All targets have been reached')
+
+        
 
 def main(args=None):
     rclpy.init(args=args)
